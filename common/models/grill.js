8a156884badb760bfd264826684ceb38fd8345c0
mislitzer <ma.is@hotmail.com>
2017-05-05 12:08:01 +0200

    Grill - findByLocation Service

--- common/models/grill.js
+++ common/models/grill.js
@@ -1,5 +1,23 @@
 'use strict';
 
 module.exports = function(Grill) {
+  Grill.findByLocation = function(cb) {
+    var response;
+    var test = true;
 
+    response = "test";
+    cb(null, response);
+  };
+  Grill.remoteMethod(
+    'findByLocation', {
+      http: {
+        path: '/findByLocation',
+        verb: 'get'
+      },
+      returns: {
+        arg: 'status',
+        type: 'string'
+      }
+    }
+  );
 };
